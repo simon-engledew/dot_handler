79cb77a137354e81e401efef6c36d36ab7e54383
Simon Engledew <simon@engledew.com>
2009-01-27 15:16:59 +0000

    now handlers multiple file types

--- lib/dot_handler.rb
+++ lib/dot_handler.rb
@@ -1,16 +1,17 @@
 class DotHandler < ActionView::TemplateHandlers::ERB
-
+  
+  Formats = Set.new(['png', 'gif'])
+  DefaultFormat = 'png'
+  
   def render(template, local_assigns = {})
-    @view.controller.headers["Content-Type"] ||= 'image/png'
+    format = DotHandler::Formats.include?(format = @view.template_format)? format : DotHandler::DefaultFormat
     
-    input = super(template)
+    @view.controller.headers["Content-Type"] ||= "image/#{format}"
     
-    output = IO.popen('dot -Tpng', 'r+') do |io|
-      io.write(input)
+    IO.popen("dot -T#{format}", 'r+') do |io|
+      io.write(super(template))
       io.close_write
       io.read
     end
-    
-    output
   end
 end
